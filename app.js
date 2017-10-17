720148239137534ec7c1fd22720c701d45dd91d0
Luis Revilla <luisrevillameza@gmail.com>
2017-10-17 18:34:06 -0300

    client side routing

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
+var path = require("path");
 var express = require("express");
 
 var app = express();
 
 app.use(express.static("build"));
+
+app.get("/*", function(req, res) {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 app.listen(3030);
